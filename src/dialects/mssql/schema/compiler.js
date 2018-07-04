6b3cea913a9c31805f251136521796d1b2e5a734
Fabian Kantereit <fabian.kantereit@inform-software.com>
2018-07-04 11:28:47 +0200

    Implement missing schema support for mssql dialect

--- src/dialects/mssql/schema/compiler.js
+++ src/dialects/mssql/schema/compiler.js
@@ -22,13 +22,13 @@ assign(SchemaCompiler_MSSQL.prototype, {
   // Rename a table on the schema.
   renameTable(tableName, to) {
     this.pushQuery(
-      `exec sp_rename ${this.formatter.parameter(tableName)}, ${this.formatter.parameter(to)}`
+      `exec sp_rename ${this.formatter.parameter(prefixedTableName(this.schema, tableName))}, ${this.formatter.parameter(to)}`
     );
   },
 
   // Check whether a table exists on the query.
   hasTable(tableName) {
-    const formattedTable = this.formatter.parameter(this.formatter.wrap(tableName));
+    const formattedTable = this.formatter.parameter(this.formatter.wrap(prefixedTableName(this.schema, tableName)));
     const sql =
       `select object_id from sys.tables ` +
       `where object_id = object_id(${formattedTable})`;
@@ -38,7 +38,7 @@ assign(SchemaCompiler_MSSQL.prototype, {
   // Check whether a column exists on the schema.
   hasColumn(tableName, column) {
     const formattedColumn = this.formatter.parameter(column);
-    const formattedTable = this.formatter.parameter(this.formatter.wrap(tableName));
+    const formattedTable = this.formatter.parameter(this.formatter.wrap(prefixedTableName(this.schema, tableName)));
     const sql =
       `select object_id from sys.columns ` +
       `where name = ${formattedColumn} ` +
